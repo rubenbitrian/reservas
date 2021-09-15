062c1b64fa97054e0ce9b1a6dcca033673d3516a
han0n <carferme@hotmail.es>
2021-09-15 11:27:32 +0200

    Implementado: Login de Google - Falta el redireccionamiento, la están las redirecciones hacia la ruta: /login --El redireccionamiento sería el dominio propio de la pagina, esto se configura desde la consola de Google. --De configurar el dominio, se debría de asignar el id del cliente web y su secreto según quién sea dueño del proyecto/web dentro de la consola de Google (reasignar los valores de .env vamos)

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,7 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+
+    Http\HttplugBundle\HttplugBundle::class => ['all' => true], // añadido
+    HWI\Bundle\OAuthBundle\HWIOAuthBundle::class => ['all' => true], // añadido
 ];

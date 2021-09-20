4c95a20f573de10a7d8d474f6e5ea30c6485cff6
han0n <carferme@hotmail.es>
2021-09-20 13:21:16 +0200

    fix en template de registro porque se cambió de nombre a name y de apellidos a surnames.
    Fix return login()

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -53,7 +53,7 @@ class SecurityController extends AbstractController
             }
         }
 
-        //return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
     /**
